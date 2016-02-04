a96f046cf28fc3f8e143768bcc977ee9130b497f
Ubuntu <ubuntu@ip-172-31-16-218.us-west-1.compute.internal>
2016-02-04 02:06:32 +0000

    streamlined the experiments

--- test_paper.sh
+++ test_paper.sh
@@ -8,19 +8,11 @@ python test_paper.py --command infer --path_conf paper/test1/b.yaml 2>&1 | \
     tee notes/1b_in.txt
 
 # test2
-#python test_paper.py --command train --path_conf paper/test2/a.yaml
-#python test_paper.py --command infer --path_conf paper/test2/a.yaml 2>&1 | \
-#    tee notes/2a_in.txt
-
 python test_paper.py --command train --path_conf paper/test2/b.yaml
 python test_paper.py --command infer --path_conf paper/test2/b.yaml 2>&1 | \
     tee notes/2b_in.txt
 
 # test3
-#python test_paper.py --command train --path_conf paper/test3/a.yaml
-#python test_paper.py --command infer --path_conf paper/test3/a.yaml 2>&1 | \
-#    tee notes/3a_in.txt
-
 python test_paper.py --command train --path_conf paper/test3/b.yaml
 python test_paper.py --command infer --path_conf paper/test3/b.yaml 2>&1 | \
     tee notes/3b_in.txt
@@ -30,10 +22,6 @@ python test_paper.py --command infer --path_conf paper/test3/c.yaml 2>&1 | \
     tee notes/3c_in.txt
 
 # test4
-#python test_paper.py --command train --path_conf paper/test4/a.yaml
-#python test_paper.py --command infer --path_conf paper/test4/a.yaml 2>&1 | \
-#    tee notes/4a_in.txt
-
 python test_paper.py --command train --path_conf paper/test4/b.yaml
 python test_paper.py --command infer --path_conf paper/test4/b.yaml 2>&1 | \
     tee notes/4b_in.txt
@@ -43,19 +31,11 @@ python test_paper.py --command infer --path_conf paper/test4/c.yaml 2>&1 | \
     tee notes/4c_in.txt
 
 # test5
-#python test_paper.py --command train --path_conf paper/test5/a.yaml
-#python test_paper.py --command infer --path_conf paper/test5/a.yaml 2>&1 | \
-#    tee notes/5a_in.txt
-
 python test_paper.py --command train --path_conf paper/test5/b.yaml
 python test_paper.py --command infer --path_conf paper/test5/b.yaml 2>&1 | \
     tee notes/5b_in.txt
 
 # test 6
-python test_paper.py --command train --path_conf paper/test6/a.yaml
-python test_paper.py --command infer --path_conf paper/test6/a.yaml 2>&1 | \
-     tee notes/6a_in.txt
-    
 python test_paper.py --command train --path_conf paper/test6/b.yaml
 python test_paper.py --command infer --path_conf paper/test6/b.yaml 2>&1 | \
     tee notes/6b_in.txt
